67bdc3d0831903a3f2199a5e3ad356a61ae0c00b
hwy <dlutweiye@gmail.com>
2020-06-02 13:14:06 +0000

    hwy commit

--- SLR_flask_server.py
+++ SLR_flask_server.py
@@ -52,10 +52,10 @@ def predict():
             with torch.no_grad():
                 prediction = model(skeleton_data_tensor)
                 pre_result = torch.max(F.softmax(prediction[:, -1, :], dim=1), 1)
-                pre_class = pre_result[1].cpu().data.numpy().tolist()
-                pre_prob = pre_result[0].cpu().data.numpy().tolist()
+                pre_class = pre_result[1].cpu().data.numpy().tolist()[0]
+                pre_prob = pre_result[0].cpu().data.numpy().tolist()[0]
             pre_class_name = class_index2name(class_dict, pre_class, dict_start_index)
-            if pre_prob > 0.9:
+            if pre_prob > 0.8:
                 result_dict["prediction"] = pre_class_name
             else:
                 result_dict["prediction"] = "Unknown"
